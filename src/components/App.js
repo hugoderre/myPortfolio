7b9b31d0264384798828c7742064937ea72950bd
Patchook <hugo.d83@outlook.fr>
2019-12-10 16:07:44 +0100

    Starting

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import Header from './header/header'
+
+class App extends Component {
+    render() {
+        return (
+            <Header />
+        )
+    }
+}
+
+export default App
