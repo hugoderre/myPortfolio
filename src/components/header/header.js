3aaa98cdc1f719d1da15d56b3f96e4c79e3a1e16
Patchook <hugo.d83@outlook.fr>
2019-12-16 13:34:56 +0100

    Add Navbar and Social Medias

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -11,7 +11,26 @@ export default class Header extends Component {
     render() {
 
         return (
-            <div></div>       
+            
+            <nav className="navbar navbar-dark bg-dark">
+                <div className="row test">
+                    <div className="col-6">
+                        <span className="navbar-brand mb-0 ml-5 h1">HUGO DERRÉ</span>
+                    </div>
+                    <div className="col-6">
+                        <div className="mb-0 text-center d-block s-m">
+                        
+                            <a href="#"><i className="btn-sm fab fa-facebook-f"></i></a>
+                            <a href="#"><i className="btn-sm fab fa-twitter"></i></a>
+                            <a href="#"><i className="btn-sm fab fa-google-plus-g"></i></a>
+                            <a href="#"><i className="btn-sm fab fa-youtube"></i></a>
+                            <a href="#"><i className="btn-sm fab fa-instagram"></i></a>
+                            
+                        </div>
+                    </div>
+                </div>
+            </nav> 
+              
         )
     }
 }
