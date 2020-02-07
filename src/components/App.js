a0a34bffc6b2fa2963b61b35132f636499166a33
Patchook <hugo.d83@outlook.fr>
2020-02-07 11:42:05 +0100

    Try to fix open graph 2.0, it would be done in 24h

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react'
 import Header from './header/header'
 import Sections from './sections/sections'
 import Footer from './footer/footer'
-import { Helmet } from "react-helmet"
-import OgImage from "../img/portfolio.png"
 
 
 
@@ -11,9 +9,6 @@ class App extends Component {
     render() {
         return (
             <div className="application">
-                <Helmet>
-                    <meta property='og:image' content={"http://hugoderre.fr" + OgImage} />
-                </Helmet>
                 <Header />
                 <Sections />
                 <Footer />
