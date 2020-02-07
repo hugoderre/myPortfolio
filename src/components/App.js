4be7e17bc77e901d0b1eb94d21933a55b16051a1
Patchook <hugo.d83@outlook.fr>
2020-02-07 11:28:10 +0100

    Try to fix open graph

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react'
 import Header from './header/header'
 import Sections from './sections/sections'
 import Footer from './footer/footer'
+import { Helmet } from "react-helmet"
+import OgImage from "../img/portfolio.png"
+
 
 
 class App extends Component {
     render() {
         return (
-            <div className="container-fluid">
+            <div className="application">
+                <Helmet>
+                    <meta property='og:image' content={"http://hugoderre.fr" + OgImage} />
+                </Helmet>
                 <Header />
                 <Sections />
                 <Footer />
             </div>
-        )
+        );
     }
 }
 
